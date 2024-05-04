9baabe22ba791134c4a761281d1522cae8244e89
DevJARM <repo.ruiz.javier@gmail.com>
2024-05-04 17:01:06 -0500

    obtener por id

--- UE.STOREDB.API/Controllers/CategoryController.cs
+++ UE.STOREDB.API/Controllers/CategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using UE.STOREDB.DOMAIN.Core.Interfaces;
 
 namespace UE.STOREDB.API.Controllers
 {
@@ -7,5 +8,28 @@ namespace UE.STOREDB.API.Controllers
     [ApiController]
     public class CategoryController : ControllerBase
     {
+        private readonly ICategoryRepository _repo;
+
+        public CategoryController(ICategoryRepository repo)
+            => _repo = repo;
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var category = await _repo.GetAll();
+
+            return Ok(category);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById (int id)
+        {
+            var category = await _repo.GetById(id);
+
+            if (category == null)
+                return NotFound();
+
+            return Ok(category);
+        }
     }
 }
