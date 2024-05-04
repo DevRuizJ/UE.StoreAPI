dcd354aba1ba187b21d81dca1d51e093e2be05e1
DevJARM <repo.ruiz.javier@gmail.com>
2024-05-04 16:01:14 -0500

    repo

---
+++ UE.STOREDB.DOMAIN/Core/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using UE.STOREDB.DOMAIN.Core.Entities;
+
+namespace UE.STOREDB.DOMAIN.Core.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<bool> Delete(int id);
+        Task<IEnumerable<Category>> GetAll();
+        Task<Category> GetById(int id);
+        Task<bool> Insert(Category category);
+        Task<bool> Update(Category category);
+    }
+}
---
+++ UE.STOREDB.DOMAIN/Infrastructure/Repositories/CategoryRepository.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UE.STOREDB.DOMAIN.Core.Entities;
+using UE.STOREDB.DOMAIN.Core.Interfaces;
+using UE.STOREDB.DOMAIN.Infrastructure.Data;
+
+namespace UE.STOREDB.DOMAIN.Infrastructure.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly StoreDbueContext _dbContext;
+
+        public CategoryRepository(StoreDbueContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        ////Sincrono
+        //public IEnumerable<Category> GetAll()
+        //{
+        //    return _dbContext
+        //            .Category
+        //            .Where(c => c.IsActive == true)
+        //            .ToList();
+        //}
+
+        public async Task<IEnumerable<Category>> GetAll()
+        {
+            return await _dbContext
+                    .Category
+                    .Where(c => c.IsActive == true)
+                    .ToListAsync();
+        }
+
+        public async Task<Category> GetById(int id)
+        {
+            return await _dbContext
+                    .Category
+                    .Where(c => c.IsActive == true && c.Id == id)
+                    .FirstOrDefaultAsync();
+        }
+
+        public async Task<bool> Insert(Category category)
+        {
+            await _dbContext.Category.AddAsync(category);
+            int rows = await _dbContext.SaveChangesAsync();
+
+            return rows > 0;
+        }
+
+        public async Task<bool> Update(Category category)
+        {
+            if (GetById(category.Id) == null)
+                return false;
+
+            _dbContext.Category.Update(category);
+            int rows = await _dbContext.SaveChangesAsync();
+
+            return rows > 0;
+        }
+
+        public async Task<bool> Delete(int id)
+        {
+            var findCategory = await _dbContext.Category
+                .Where(c => c.IsActive == true && c.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (findCategory == null)
+                return false;
+
+            _dbContext.Category.Remove(findCategory);
+
+            int rows = await _dbContext.SaveChangesAsync();
+
+            return rows > 0;
+        }
+    }
+}
